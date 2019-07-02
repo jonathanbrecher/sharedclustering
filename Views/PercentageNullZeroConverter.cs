ee3b3d710d1767b5c1aed6394b9175d9deb20eff
Jonathan Brecher <8268937+jonathanbrecher@users.noreply.github.com>
2019-07-01 23:20:12 -0400

    Display progress percentage as blank if zero.

---
+++ Views/PercentageNullZeroConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace AncestryDnaClustering.Views
+{
+    /// <summary>
+    /// Display a value as percentage if non-zero, blank if zero
+    /// </summary>
+    internal class PercentageNullZeroConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return System.Convert.ToDouble(value) == 0.0 ? null: string.Format(culture, "{0:P2}", value);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
