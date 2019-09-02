d7e105765d4a70640fc1533664ecf293ecd9cc40
Jonathan Brecher <8268937+jonathanbrecher@users.noreply.github.com>
2019-09-02 16:17:11 -0400

    Optionally show shared match counts when probing endogamy.

--- Models/EndogamyProber.cs
+++ Models/EndogamyProber.cs
@@ -3,6 +3,7 @@ using System;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Input;
 
 namespace AncestryDnaClustering.Models
 {
@@ -17,6 +18,8 @@ namespace AncestryDnaClustering.Models
 
         public async Task ProbeAsync(string name, string guid, int matchIndexTarget, int numMatchesToTest, Throttle throttle, ProgressData progressData)
         {
+            var shiftKeyDown = Keyboard.IsKeyDown(Key.LeftShift) || Keyboard.IsKeyDown(Key.RightShift);
+
             var pageNum = matchIndexTarget / _matchesRetriever.MatchesPerPage + 1;
             var matches = await _matchesRetriever.GetMatchesPageAsync(guid, pageNum, false, throttle, progressData);
             var icwTasks = matches
@@ -24,35 +27,38 @@ namespace AncestryDnaClustering.Models
                 .Select(match => _matchesRetriever.GetRawMatchesInCommonAsync(guid, match.TestGuid, 1, 0, throttle))
                 .ToList();
             var icws = await Task.WhenAll(icwTasks);
-            var medianIcwCount = icws.Select(l => l.Count).Skip(icwTasks.Count() / 2).FirstOrDefault();
+            var orderedIcwCounts = icws.Select(l => l.Count).OrderBy(c => c).ToList();
+            var medianIcwCount = orderedIcwCounts.Skip(orderedIcwCounts.Count() / 2).FirstOrDefault();
+
+            string primaryMessage;
+            string secondaryMessage;
 
             if (medianIcwCount <= 20)
             {
                 // Median 0-20: No endogamy
-                MessageBox.Show($"The test results for {name} do not show any significant endogamy. " +
-                    Environment.NewLine +
-                    Environment.NewLine +
-                    $"Clustering should work well.",
-                    "Endogamy test", MessageBoxButton.OK, MessageBoxImage.Information);
+                primaryMessage = $"The test results for {name} do not show any significant endogamy. ";
+                secondaryMessage = "Clustering should work well.";
             }
             else if (medianIcwCount < _matchesRetriever.MatchesPerPage * 3 / 4)
             {
                 // Median 20-150: Some endogamy but probably not all lines
-                MessageBox.Show($"The test results for {name} may show some endogamy in some ancestral lines. " +
-                    Environment.NewLine +
-                    Environment.NewLine +
-                    $"Clustering should work well for the ancestral lines without endogamy, but may be difficult to interpret for others.",
-                    "Endogamy test", MessageBoxButton.OK, MessageBoxImage.Information);
+                primaryMessage = $"The test results for {name} may show some endogamy in some ancestral lines. ";
+                secondaryMessage = "Clustering should work well for the ancestral lines without endogamy, but may be difficult to interpret for others.";
             }
             else
             {
                 // Median 150-200: Heavy endogamy
-                MessageBox.Show($"The test results for {name} show significant endogamy. " +
-                    Environment.NewLine +
-                    Environment.NewLine +
-                    $"Clustering by itself may not work well. Considering using the 'Endogamy special' downloading option combined with Similarity to find more distant matches.",
-                    "Endogamy test", MessageBoxButton.OK, MessageBoxImage.Information);
+                primaryMessage = $"The test results for {name} show significant endogamy. ";
+                secondaryMessage = "Clustering by itself may not work well. Considering using the 'Endogamy special' downloading option combined with Similarity to find more distant matches.";
             }
+
+            if (shiftKeyDown)
+            {
+                secondaryMessage = secondaryMessage + Environment.NewLine + Environment.NewLine + string.Join(", ", orderedIcwCounts);
+            }
+
+            MessageBox.Show(primaryMessage + Environment.NewLine + Environment.NewLine + secondaryMessage,
+                "Endogamy test", MessageBoxButton.OK, MessageBoxImage.Information);
         }
     }
 }
